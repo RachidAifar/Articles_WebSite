41fe57e9838c24702ee711793e07729910a0691b
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2022-03-11 08:05:42 +0100

    Feat:organize the file on folders

---
+++ Project_Files/pages/home.php
@@ -0,0 +1,14 @@
+<h1>
+    Home page
+</h1>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptatem neque sunt ut voluptas soluta nostrum commodi, vitae enim, deleniti sed inventore fuga nemo quia repellat repellendus. Animi, corporis odit?</p>
+<p>Quis unde debitis itaque. Alias eum asperiores deserunt laborum aliquid, atque, nemo unde fuga consequatur doloribus maiores. Doloremque ipsa qui unde, optio maiores, sapiente alias, commodi ipsam dolore distinctio asperiores!</p>
+<p>Pariatur eos assumenda voluptatibus cum reprehenderit corporis, laborum sed veniam laudantium quisquam, accusantium odit maxime sequi eaque molestiae corrupti veritatis totam aliquam! Consectetur a ipsa nesciunt cum placeat doloribus nobis.</p>
+<p>Itaque veniam aut voluptatem nemo temporibus sit cum consectetur? Odio exercitationem, eos asperiores eveniet commodi praesentium at mollitia animi molestias, officiis omnis harum quod ullam dolores! Ipsam quo fuga mollitia.</p>
+<p>Fugit error velit nihil. Voluptates obcaecati vitae ullam deleniti, magni beatae cum amet numquam ipsa quasi distinctio omnis, consectetur expedita esse dignissimos officia totam iure? Blanditiis incidunt neque ipsum. Reiciendis.</p>
+<p>Laudantium vero porro exercitationem autem, officia vitae libero doloribus repudiandae aliquid magnam sapiente pariatur laboriosam quidem voluptate, consequatur reiciendis dignissimos deserunt tempora. Ipsa ducimus adipisci quibusdam ea possimus, earum iste.</p>
+<p>Blanditiis quasi voluptatum aliquam dolor perspiciatis aperiam optio esse earum quod excepturi nemo accusantium, nulla distinctio consectetur, ratione sint nam doloribus, praesentium aliquid fuga quam fugiat in hic autem! Quibusdam!</p>
+<p>Nobis repellendus quas, vitae ipsa culpa perspiciatis harum officiis voluptates possimus dolores vero nisi tempora aut minus enim quos eaque dolore consectetur dignissimos iste consequuntur hic libero corrupti. Aperiam, iusto.</p>
+<p>Debitis fugiat atque ratione ipsam quisquam magni hic est illum maxime soluta possimus, optio incidunt. Deleniti ab architecto esse sequi iure libero facilis sed placeat soluta impedit? Recusandae, expedita nisi?</p>
+<p>Asperiores sunt qui non assumenda velit perferendis sapiente architecto aliquam, animi reprehenderit libero ipsa eveniet fugit unde. Impedit nulla nemo aspernatur magnam? Ex recusandae commodi suscipit deserunt inventore autem. Molestias!</p>
