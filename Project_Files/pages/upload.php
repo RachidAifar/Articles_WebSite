d87a9f0bd7b7971ab9dceb9259b82c7ffbe746d4
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2022-03-16 08:17:49 +0100

    Feat:css style added to upload page

---
+++ Project_Files/pages/upload.php
@@ -0,0 +1,30 @@
+
+
+
+<div class="page-upload">
+    <form class="card" action="">
+        <div>
+            <label for="title">Title</label>
+            <input type="text" name="title">
+        </div>
+        <div>
+            <label for="subject">Subject</label>
+            <input type="text" name="subject">
+        </div>
+        <div>
+            <label for="author">Author</label>
+            <input type="text" name="author">
+        </div>
+        <div>
+            <label for="description">Discription</label>
+            <textarea name="description" ></textarea>
+        </div>
+        <div>
+            <label for="year">Year</label>
+            <input type="number" name="year">
+        </div>
+        <div>
+            <button type="submit">Upload</button>
+        </div>
+    </form>
+</div>
