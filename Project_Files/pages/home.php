4c12a2ca9f94e2dff93467cccea6c64707749ef5
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2022-04-18 12:57:00 +0200

    Feat:image and pagination added to home page

--- Project_Files/pages/home.php
+++ Project_Files/pages/home.php
@@ -1,6 +1,10 @@
 <?php 
+    $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
+    $limit = ($current_page - 1) * PAGE_LIMIT;
+
 
     $articles =get_articles_list();
+    $total_pages = ceil(get_article_count() / PAGE_LIMIT);
 ?>
 
 
@@ -10,8 +14,21 @@
     Home page
 </h1>
 
-<?php while ($article =$articles ->Fetch_assoc()) : ?>
-    <div>
-        <?= $article['title'];?>
-    </div>
-    <?php endwhile;?> 
+
+<div class="article-list">
+    <?php while ($article =$articles ->Fetch_assoc()) : ?>
+        <div class="article-item">
+            <a href="<?= page_url('details',['id'=>$article['id']])?>">
+                 <img src="https://via.placeholder.com/150" alt="   <?= $article['title'];?>">
+            </a>
+            <?= $article['title'];?>(<?= $article['subject'];?>)
+        </div>
+    <?php endwhile;?>
+    <nav class="pagination">
+    <?php for ($i = 1; $i <= $total_pages; $i++) : ?>
+        <a href="<?= page_url('home', ['page' => $i]); ?>" <?= $i == $current_page ? 'class="active"' : ''; ?>>
+            <?= $i; ?>
+        </a>
+    <?php endfor; ?>
+</nav> 
+</div>
