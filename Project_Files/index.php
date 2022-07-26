86498929fc985673e75c4fceecae8392d6a5716f
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2022-07-26 14:00:35 +0200

    Feat:Final touches on the project

--- Project_Files/index.php
+++ Project_Files/index.php
@@ -1,11 +1,13 @@
 <?php 
 
+session_start();
+define('APP_VERSION','1.0.0');
 //isset it check whether the variable is exist inside set or not
 //and it return true if it's yes and false if it's not
 require_once "./config.php";
 require_once "./functions.php";
 
-define('APP_VERSION','1.0.0');
+
 
 $db = db_connect();
 
@@ -16,16 +18,13 @@ $errors=[];
 
 //if this function it's already included on other place
                              //it will not include again 
-                            //so it make sure it doesn't include multiple times
+                            //so it makes sure it doesn't include multiple times
                             
 if(file_exists("./pages/{$page}.php")){
     include "./pages/{$page}.php";
 }else{
     include './pages/404.php';
 }
-
-
-
 db_close($db);
 
  
